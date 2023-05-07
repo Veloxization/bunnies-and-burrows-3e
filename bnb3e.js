8c9deb2651c6252b23efdb3e8f7cda5f0d32d523
Veloxization <github@velo.anonaddy.com>
2023-05-07 19:22:11 +0300

    Fixed bnb3e.js formatting

--- bnb3e.js
+++ bnb3e.js
@@ -3,9 +3,10 @@ import BnBActorSheet from "./modules/actor/sheet/bnb-actor-sheet.js";
 Hooks.once("init", function() {
     console.log("BnB3e | Initializing Bunnies and Burrows 3e System")
 
-    Actors.unregisterSheet("core", ActorSheet)
+    Actors.unregisterSheet("core", ActorSheet);
     Actors.registerSheet(
         game.system.id,
         BnBActorSheet,
-        { label: game.i18n.localize("BNB.Sheets.CharacterSheet"), types: ["character"], makeDefault: true })
+        { label: game.i18n.localize("BNB.Sheets.CharacterSheet"), types: ["character"], makeDefault: true }
+    );
 });
